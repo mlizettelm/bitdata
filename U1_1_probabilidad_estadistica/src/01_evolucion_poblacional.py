a97ddfd71be953f5b3fb846f2e391b8a03a4894d
LizL <mlm107311@lasalle.edu.mx>
2026-08-29 16:23:26 -0600

    reconstruccion del repositorio

---
+++ U1_1_probabilidad_estadistica/src/01_evolucion_poblacional.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np  
+
+df = pd.read_csv("/Users/lizette/Coding/bigdata/U1_1_probabilidad_estadistica/data/raw/annual-population-growth/annual-population-growth.csv")
+
+print(df.head(3))
